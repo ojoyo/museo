04e52884c299239ac585a8da088ce26028a042c4
Sebastian Petryna <sp360609@students.mimuw.edu.pl>
2016-04-18 22:06:53 +0200

    dodany bluetooth popup

--- BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/MainActivity.java
+++ BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/MainActivity.java
@@ -1,25 +1,66 @@
 package org.beaconmuseum.beaconmuseum;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kontakt.sdk.android.common.KontaktSDK;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter btAdapter;
+
+    BroadcastReceiver bluetoothState = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String stateExtra = BluetoothAdapter.EXTRA_STATE;
+            int state = intent.getIntExtra(stateExtra, -1);
+            String toastText = "";
+            switch (state) {
+                case BluetoothAdapter.STATE_ON:
+                    toastText = "Bluetooth on";
+                    Toast.makeText (MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    toastText = "Bluetooth off";
+                    Toast.makeText (MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkBluetoothConnection();
+
         // Test if compiles:
         // KontaktSDK.initialize("YOUR API KEY");
 
         BeaconManager.initialize(this);
+    }
+
+    private void checkBluetoothConnection() {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!btAdapter.isEnabled()) {
+            String actionStateChanged = BluetoothAdapter.ACTION_STATE_CHANGED;
+            String actionRequestEnable = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+            IntentFilter filter = new IntentFilter(actionStateChanged);
+            registerReceiver(bluetoothState, filter);
+            startActivityForResult(new Intent(actionRequestEnable), 0);
+        }
     }
 
     public void refreshClick(View v) {
