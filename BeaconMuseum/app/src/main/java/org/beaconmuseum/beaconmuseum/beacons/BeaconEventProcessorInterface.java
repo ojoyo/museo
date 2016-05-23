7efdb9e0cb3a29ff3330956fea6c4eb5b61bbd10
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2016-05-23 02:11:08 +0200

    Nicer GUI added, manifest, activity_main, MainActivity modified

--- BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/beacons/BeaconEventProcessorInterface.java
+++ BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/beacons/BeaconEventProcessorInterface.java
@@ -3,5 +3,5 @@ package org.beaconmuseum.beaconmuseum.beacons;
 import com.kontakt.sdk.android.ble.discovery.EventType;
 
 public interface BeaconEventProcessorInterface {
-    public void processBeaconEvent(EventType event, BeaconInfo beacon);
+    void processBeaconEvent(EventType event, BeaconInfo beacon);
 }
