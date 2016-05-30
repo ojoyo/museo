46cf8caa5b0ce557a321076c41f86f369130d1f6
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2016-05-30 21:09:51 +0200

    pulling

--- BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/MainActivity.java
+++ BeaconMuseum/app/src/main/java/org/beaconmuseum/beaconmuseum/MainActivity.java
@@ -44,8 +44,6 @@ public class MainActivity extends RoboActivity implements BeaconEventProcessorIn
         closestPainting.loadUrl("https://pl.wikipedia.org/wiki/Zdzis%C5%82aw_Beksi%C5%84ski");
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
